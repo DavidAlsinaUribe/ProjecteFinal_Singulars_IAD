8aad0c46b7648f8e5e8831539efc44e7f9f20d2a
David <davidalsina1996@gmail.com>
2021-02-24 14:39:21 +0100

    Web Modification + Parallax exemple

--- web.js
+++ web.js
@@ -3,27 +3,49 @@
 /* LightBox -> Tarjetes presentació
 ------------------------------------------------------ */
 
-var obrir = document.querySelector('#obrir')
-var tancar = document.querySelector('#tancar')
-var lightbox= document.querySelector('#lightbox')
-var blackOverlay = document.querySelector('#blackOverlay')
+// var obrir = document.querySelector('#obrir')
+// var tancar = document.querySelector('#tancar')
+// var lightbox= document.querySelector('#lightbox')
+// var blackOverlay = document.querySelector('#blackOverlay')
 
-obrir.addEventListener("click", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    blackOverlay.style.display = "block"
-})
+// obrir.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     blackOverlay.style.display = "block"
+// })
 
-tancar.addEventListener("click", (e) => {
-    e.preventDefault();
-    blackOverlay.style.display = "none"
-})
+// tancar.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     blackOverlay.style.display = "none"
+// })
 
-window.addEventListener("click", (e) => {
-    if (!lightbox.contains(e.target)){
-        blackOverlay.style.display = "block"
-    }
-})
+// window.addEventListener("click", (e) => {
+//     if (!lightbox.contains(e.target)){
+//         blackOverlay.style.display = "block"
+//     }
+// })
+
+/*----------------------------------------------------*/
+/* Parallax -> Moviment LEGOS
+------------------------------------------------------ */
+
+window.addEventListener("mousemove", (e) => {
+    console.log("ok");
+    parallax(e);
+});
+function parallax(e) {
+    layer = document.querySelector('.layer');
+
+    const speed = layer.getAttribute('data-speed');
+    
+    const x = (e.pageX * speed)/100;
+    const y = (e.pageY * speed)/100;
+    
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+}
+
+
+
 
 
 
